daf2205cba30fe314ec29a77a8208b9ae7067815
zhou <yu.zhou@bausch.com>
2022-04-19 15:31:45 +0800

    p12_zyn

--- period12.py
+++ period12.py
@@ -14,6 +14,6 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 
 driver.get('http://localhost:63342/projectAutoTest/projectHtml/chapter3/period12/period12.html')
 time.sleep(1)
-driver.find_element_by_id('downloadFile').click()
+
 
 # driver.quit()
